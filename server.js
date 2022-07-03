f2a867f576e53663b37a3e48b0abc70370ab1cff
Katrina  Shih <katrinashih@Katrinas-MacBook-Pro.local>
2022-07-02 23:27:44 -0400

    modified server.js file to match with routes.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./route/apiroute');
+const htmlRoutes = require('./route/htmlroute');
 
 app.use(express.static('public'));
 // parse incoming string or array data
@@ -17,4 +17,3 @@ app.use('/', htmlRoutes);
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}. Welcome!`);
   });
-Footer
